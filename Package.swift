87576a0a50856c45100191fb8fc3c451d79b2deb
Vlad Papko <uladzimir.papko@gmail.com>
2020-11-12 12:46:03 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
-  name: "TransitionButton"
+    name: "TransitionButton",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "TransitionButton",
+            targets: ["TransitionButton"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "TransitionButton"
+        )
+    ]
 )
